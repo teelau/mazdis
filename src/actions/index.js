2614e3379b5c8ca3301e803f3c4d9bbaddb85af9
bohu8 <bohu8@hotmail.com>
2018-01-30 00:28:07 -0800

    abstract POST handler function and make register use it

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,65 @@ export var firebase_app = firebase.initializeApp(FIREBASE_CONFIG);
 export var firebase_db = firebase.database();
 
 export default firebase_app;
+
+export function getAuth(auth) {
+    if (auth){
+        return firebase_app.auth().currentUser.getIdToken(true);
+    } else {
+        return Promise.resolve('');
+    }
+}
+
+export function createDispatcherForPost(url, body, {actionLabel, successLabel, invalidLabel, failureLabel}, options = {}){
+    const { auth, responseJsonFunc, headers } = options;
+    let reqHeaders = {
+        ...headers,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    };
+
+    return (dispatch) => {
+        dispatch({type: actionLabel});
+        getAuth(auth)
+            .then((token) => {
+                if (token) {
+                    reqHeaders.Authorization = `Bearer ${token}`;
+                }
+                return fetch(url, {
+                    method: 'POST',
+                    headers: reqHeaders,
+                    body: JSON.stringify(body),
+                });
+            }).then((response) => {            
+                if(response.status == 200 || response.status == 500){
+                // TODO: api currently returns HTTP 500 for invalid registration, should be changed to 4xx.
+                    return response.json();
+                } else { // Reserved for offline or unknown errors, typically HTTP 500
+                    return new Promise.reject({error: response});
+                }
+            }).then((responseJson) => {
+                if(responseJson.message) {
+                    // POST request validation error
+                    return dispatch({
+                        type: invalidLabel,
+                        payload: {
+                            message: responseJson.message,
+                        },
+                    });
+                } else {
+                    // successful POST request
+                    return dispatch({
+                        type: successLabel,
+                        payload: responseJsonFunc ?
+                            responseJsonFunc(responseJson) :
+                            responseJson,
+                    });
+                }
+            }).catch((error) => {
+                return dispatch({
+                    type: failureLabel,
+                    payload: error,
+                });
+            });
+    };
+}
