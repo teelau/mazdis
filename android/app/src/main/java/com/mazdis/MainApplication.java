5f88b421a6ec208ec0ad4d7be88c96bbb7c21af1
bohu8 <bohu8@hotmail.com>
2018-04-02 01:26:10 -0700

    link react native community icons

--- android/app/src/main/java/com/mazdis/MainApplication.java
+++ android/app/src/main/java/com/mazdis/MainApplication.java
@@ -3,6 +3,8 @@ package com.mazdis;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new MapsPackage()
       );
